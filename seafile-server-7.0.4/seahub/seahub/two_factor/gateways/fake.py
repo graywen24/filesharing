66617a2be10eaad36b189155b12690d84af75553
graywen24 <deutschland.gray@gmail.com>
2019-10-03 08:11:10 +0000

    enable 3rd party sms gateway function

--- seafile-server-7.0.4/seahub/seahub/two_factor/gateways/fake.py
+++ seafile-server-7.0.4/seahub/seahub/two_factor/gateways/fake.py
@@ -1,5 +1,8 @@
 # Copyright (c) 2012-2016 Seafile Ltd.
 import logging
+import requests
+
+from django.utils.translation import ugettext, pgettext
 
 logger = logging.getLogger(__name__)
 
@@ -33,4 +36,50 @@ class Fake(object):
 
     @staticmethod
     def send_sms(device, token):
+        logger.info('here show =========')
         logger.info('Fake SMS to %s: "Your token is: %s"', device.number, token)
+        logger.info('Fake SMS device is  %s',device)
+        logger.debug('Fake SMS to %s: "Your token is: %s"', device.number, token)
+
+
+class Onewaysms(object):
+    """
+    apiusername=API79OLFWW2JC
+    apipassword=API79OLFWW2JCTKH77
+    mobileno=device.number
+    senderid=1-Net_SG
+    languagetype=1 for  en
+    message=token
+
+
+    def send_sms(self, device, token):
+        logging.info("twillow token is %s", token)
+        body = ugettext('Your authentication token is %s') % token
+        self.client.sms.messages.create(
+            to=device.number,
+            from_=getattr(settings, 'TWILIO_CALLER_ID'),
+            body=body)
+
+    """
+
+    @staticmethod
+    def send_sms(device, token):
+        url = "http://gateway.onewaysms.sg:10002/api.aspx"
+        messagecontent = ugettext('Your authentication token is %s') % token
+
+        values = {
+            'mobileno': device.number,
+            'message': messagecontent,
+            'apiusername':'API79OLFWW2JC',
+            'apipassword':'API79OLFWW2JCTKH77',
+            'senderid':'1- Net_SG',
+            'languagetype':'1'
+        }
+        #requests.post()
+        
+        x = requests.get(url, params=values)
+        logger.info('get string result %s' , x)
+        logger.info('get request result %s' , x.text)
+        logger.info('line 80 Oneway SMS to %s: "Your token is: %s"', device.number, token)
+
+
