eb9471e9eed4b2f2049744bbeecdab100d2da1e2
Wen Wen <deutschland.gray@gmail.com>
2019-11-18 17:36:45 +0800

    Update onewaysms.py
    remove apiuser password

--- seafile-pro-server-7.0.10/seahub/seahub/two_factor/gateways/onewaysms.py
+++ seafile-pro-server-7.0.10/seahub/seahub/two_factor/gateways/onewaysms.py
@@ -26,8 +26,8 @@ class Onewaysms(object):
         values = {
             'mobileno': device.number,
             'message': messagecontent,
-            'apiusername':'API79OLFWW2JC',
-            'apipassword':'API79OLFWW2JCTKH77',
+            'apiusername':'xxxxxx',
+            'apipassword':'xxxxxx',
             'senderid':'1- Net_SG',
             'languagetype':'1'
         }
